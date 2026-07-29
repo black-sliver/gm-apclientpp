66007ec58fa26a45b160482c5ed06711b0c8952c
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-07-29 22:19:02 +0200

    test: sort includes

--- test/test.c
+++ test/test.c
@@ -1,8 +1,9 @@
-#include "../include/gm-apclientpp.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "../include/gm-apclientpp.h"
 
 
 #define GAME "Test"
